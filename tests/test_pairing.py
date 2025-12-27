37f7e11fd406a0fbb75ee56f5f3f77e17fe8d763
sbezo <stefan.bezo@gmail.com>
2025-12-27 11:18:23 +0100

    print to pdf

--- tests/test_pairing.py
+++ tests/test_pairing.py
@@ -27,26 +27,26 @@ def test_page_load(driver):
     driver.get(BASE_URL)
     assert "Add Player to Table" in driver.page_source
 
-def add_players(driver):
-    # add players
+def test_palyers_table(driver):
+    # add 2 players to table
     driver.get(BASE_URL)
     player_input = driver.find_element(By.ID, "name")
     player_input.send_keys("Magnus Carlsen")
     player_input = driver.find_element(By.ID, "Elo")
     player_input.send_keys("2822")
-    add_button = driver.find_element(By.CSS_SELECTOR, "div#tab1 button")
+    add_button = driver.find_element(By.ID, "AddPlayer")
     add_button.click()
     player_input = driver.find_element(By.ID, "name")
     player_input.send_keys("Hikaru Nakamura")
     player_input = driver.find_element(By.ID, "Elo")
-    player_input.send_keys("2811")
+    player_input.send_keys("2801")
+    add_button = driver.find_element(By.ID, "AddPlayer")
     add_button.click()
-
     ## Check players appear
     cells = driver.find_elements(By.TAG_NAME, "td")
-    texts = [c.text for c in cells]
-    assert "Magnus Carlsen" in texts
-    assert "Hikaru Nakamura" in texts
-    assert "2822" in texts
-    assert "2811" in texts
+    assert any("Magnus Carlsen" in cell.text for cell in cells)
+    assert any("2822" in cell.text for cell in cells)
+    assert any("Hikaru Nakamura" in cell.text for cell in cells)
+    assert any("2801" in cell.text for cell in cells)
+
 
